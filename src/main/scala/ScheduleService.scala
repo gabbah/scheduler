6b6c6ebd025b737f5a1937d0cbcd0fc38acb4225
gabbah <gabriel.klappenbach@gmail.com>
2017-04-01 17:50:06 +0200

    refactor

--- src/main/scala/ScheduleService.scala
+++ src/main/scala/ScheduleService.scala
@@ -5,6 +5,7 @@ import com.twitter.finagle.http.Status
 import io.circe.generic.auto._
 import io.finch.circe._
 import io.finch._
+import shapeless.HNil
 
 case class Test(content: String)
 case class Session(id: UUID, title: Title, owner: UserId)
@@ -20,24 +21,26 @@ case class CreateSessionPayload(userId: UserId, title: Title)
 sealed trait SessionStatus
 case object Locked extends SessionStatus
 case object Open extends SessionStatus
+case class Schema()
 
 case class FinchScheduleService() {
-  private val userId: Endpoint[UserId] = uuid.map(UserId)
-  private val sessionId: Endpoint[SessionId] = uuid.map(SessionId)
+  private val userId = uuid.as[UserId]
+  private val session = "sessions" :: uuid.as[SessionId]
 
-  private val sessionE = "sessions" :: sessionId
   val api =
-    post("sessions" :: jsonBody[CreateSessionPayload] )       { createSession     } :+:
-    get (sessionE :: "status")                                { getSessionStatus  } :+:
-    put (sessionE :: "status" :: jsonBody[Status])            { setSessionStatus  } :+:
-    get (sessionE :: "topics" )                               { listTopics        } :+:
-    post(sessionE :: "topics" :: jsonBody[Topic])             { createTopic       } :+:
-    put (sessionE :: "votes"  :: userId :: jsonBody[Vote])    { placeVote         }
+    post("sessions" :: jsonBody[CreateSessionPayload] )      { createSession     } :+:
+    get (session :: "status")                                { getSessionStatus  } :+:
+    get (session :: "schema")                                { getSchema         } :+:
+    put (session :: "status" :: jsonBody[Status])            { setSessionStatus  } :+:
+    get (session :: "topics" )                               { listTopics        } :+:
+    post(session :: "topics" :: jsonBody[Topic])             { createTopic       } :+:
+    put (session :: "votes"  :: userId :: jsonBody[Vote])    { placeVote         }
 
 
   private def createSession = (payload: CreateSessionPayload) => Ok(Session(UUID.randomUUID(), payload.title, owner = payload.userId))
-  private def getSessionStatus = (sessionId: SessionId) => Ok(Locked)
+  private def getSessionStatus = (sessionId: SessionId) => Output.unit(Status.Locked)
   private def setSessionStatus = (sessionId: SessionId, status: Status) => Ok()
+  private def getSchema = (sessionId: SessionId) =>  Output.unit(Status.Locked)
   private def createTopic = (sessionId: SessionId, topic: Topic) => Ok()
   private def listTopics = (sessionId: SessionId) => Ok(Seq.empty[Topic])
   private def placeVote = (sessionId: SessionId, userId: UserId, vote: Vote) => Ok()
