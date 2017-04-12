4ee95937af105aa91be4b31a3d88c58fd438b4d8
gabbah <gabriel.klappenbach@gmail.com>
2017-04-12 21:14:13 +0200

    get Session and create Topic implemented

--- src/main/scala/Repository.scala
+++ src/main/scala/Repository.scala
@@ -1,8 +1,10 @@
 import java.util.UUID
 
 import com.outworkers.phantom.dsl._
+import com.outworkers.phantom.dsl.ResultSet
 import com.outworkers.phantom.finagle._
 import com.twitter.util
+import com.twitter.util.Future
 
 object Defaults {
   val hosts = Seq("127.0.0.1")
@@ -11,21 +13,40 @@ object Defaults {
 
 class Sessions extends CassandraTable[Sessions, Session] {
   object id extends UUIDColumn(this) with PartitionKey
-  object owner extends UUIDColumn(this) with PrimaryKey
+  object owner extends UUIDColumn(this)
   object title extends StringColumn(this)
   object resources extends MapColumn[UUID, String](this)
   object timeSlots extends IntColumn(this)
   object status extends StringColumn(this)
+
+  override def fromRow(r: Row): Session =
+    Session(
+      id(r),
+      Title(title(r)),
+      UserId(owner(r)),
+      resources(r).map{ case (uuid: UUID, s: String) =>  Resource(uuid,s)}.toSeq,
+      timeSlots(r),
+      status(r) match { case "Open" => Open; case "Locked" => Locked}
+    )
+}
+
+class Topics extends CassandraTable[Topics, Topic] {
+  object session_id extends UUIDColumn(this) with PartitionKey
+  object id extends UUIDColumn(this) with PrimaryKey
+  object title extends StringColumn(this)
+  object description extends StringColumn(this)
 }
 
 abstract class MySessionsTable extends Sessions with RootConnector
+abstract class MyTopicsTable extends Topics with RootConnector
 
 class Repository(override val connector: KeySpaceDef) extends Database[Repository](connector) {
   implicit val s = connector.session
 
   object sessions extends MySessionsTable with connector.Connector
+  object topics extends MyTopicsTable with connector.Connector
 
-  def store(session: Session): util.Future[ResultSet] =
+  def store(session: Session): Future[Session] = {
     sessions.insert
       .value(_.id, session.id)
       .value(_.resources, session.resources.map(r => (r.id, r.name)).toMap)
@@ -34,6 +55,22 @@ class Repository(override val connector: KeySpaceDef) extends Database[Repositor
       .value(_.status, session.status.toString)
       .value(_.timeSlots, session.timeSlots)
       .execute()
+      .map { case r: ResultSet => session}
+  }
+
+  def store(topic: Topic): Future[Topic] = {
+    topics.insert
+    .value(_.session_id, topic.sessionId)
+    .value(_.id, topic.id)
+    .value(_.title, topic.title.title)
+    .value(_.description, topic.description)
+    .execute()
+    .map { case r: ResultSet => topic}
+
+  }
+
+  def get(sessionId: UUID): util.Future[Option[Session]] =
+    sessions.select.where(_.id eqs sessionId).get()
 }
 
 object Repository extends Repository(Defaults.connector)
--- src/main/scala/ScheduleService.scala
+++ src/main/scala/ScheduleService.scala
@@ -9,7 +9,7 @@ import io.finch._
 
 case class Resource(id: UUID, name: String)
 case class Session(id: UUID, title: Title, owner: UserId, resources: Seq[Resource], timeSlots: Int, status: SessionStatus)
-case class Topic(id: UUID, title: Title, description: String)
+case class Topic(id: UUID, sessionId: UUID, title: Title, description: String)
 case class Vote(userId: UserId, list: Seq[Topic])
 case class UserId(id: UUID)
 case class SessionId(id: UUID)
@@ -18,6 +18,8 @@ case class Title(title: String){
 }
 
 case class CreateSessionPayload(userId: UUID, title: String, resources: Seq[String], timeSlots: Int)
+case class CreateTopicPayload(title: String, description: String)
+
 sealed trait SessionStatus
 case object Locked extends SessionStatus
 case object Open extends SessionStatus
@@ -33,7 +35,7 @@ case class FinchScheduleService(repo: Repository) {
     get (session :: "schema")                                { getSchema         } :+:
     put (session :: "status" :: jsonBody[Status])            { setSessionStatus  } :+:
     get (session :: "topics" )                               { listTopics        } :+:
-    post(session :: "topics" :: jsonBody[Topic])             { createTopic       } :+:
+    post(session :: "topics" :: jsonBody[CreateTopicPayload]){ createTopic       } :+:
     put (session :: "votes"  :: userId :: jsonBody[Vote])    { placeVote         }
 
 
@@ -45,11 +47,17 @@ case class FinchScheduleService(repo: Repository) {
       payload.resources.map(Resource(UUID.randomUUID(), _)),
       payload.timeSlots,
       Open)
-  ).map(_ =>  Ok() ) //.rescue(Future(Conflict(_)))
+  ).map{
+    case session =>  Ok(session)
+  }
+
   def getSessionStatus = (sessionId: SessionId) => Future(Output.unit(Status.Locked))
   def setSessionStatus = (sessionId: SessionId, status: Status) => Future(Ok())
   def getSchema = (sessionId: SessionId) =>  Future(Output.unit(Status.Locked))
-  def createTopic = (sessionId: SessionId, topic: Topic) => Future(Ok())
+  def createTopic = (sessionId: SessionId, payload: CreateTopicPayload) => repo.get(sessionId.id) flatMap {
+      case Some(_) => repo.store(Topic(UUID.randomUUID(), sessionId.id, Title(payload.title) , payload.description)).map(Ok)
+      case None => Future(NotFound(new IllegalArgumentException("session not found")))
+    }
   def listTopics = (sessionId: SessionId) => Future(Ok(Seq.empty[Topic]))
   def placeVote = (sessionId: SessionId, userId: UserId, vote: Vote) => Future(Ok())
 
